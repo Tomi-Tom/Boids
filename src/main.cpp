8e5f3eefaf1a6a9b75e68dca6cc442812aa66236
Tom BARITEAU-PETER <tom.bariteau-peter@epitech.eu>
2023-03-25 11:23:34 +0100

    feat: add somes functions in main to test SFML

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,24 @@
 #include <cstdlib>
 #include <iostream>
 #include <ctime>
+#include <SFML/Graphics.hpp>
 
 int main(int argc, char const **argv, char const **env)
 {
+    sf::RenderWindow window(sf::VideoMode(1920, 1020), "SFML window");
+    sf::Texture texture;
+    texture.loadFromFile("assets/basic_boid.png");
+    sf::Sprite sprite(texture);
+    sf::Event event;
+
     std::cout << "Hello World !" << std::endl;
+    while (window.isOpen()) {
+        while (window.pollEvent(event))
+            if (event.type == sf::Event::Closed)
+                window.close();
+        window.clear();
+        window.draw(sprite);
+        window.display();
+    }
     return 0;
 }
